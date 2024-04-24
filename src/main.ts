92a7d5e0373c1a1e0ee5c73d096520569f7f64ed
Weeraphat2000 <weeraphat.main@gmail.com>
2024-04-24 14:57:37 +0700

    cors setGlobalPrefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //
+  //
+  // app.use(cors()); // ต้องใช้นี่ด้วย
+  // app.enableCors(); // เหมือนใช้ cors ของ express
+  // ใน Nest.js, app.enableCors() และ app.use(cors()) มีหน้าที่เหมือนกัน คือการเปิดใช้งาน CORS (Cross-Origin Resource Sharing) เพื่ออนุญาตให้แอปพลิเคชันของคุณรับคำขอจากโดเมนอื่น ๆ โดยที่มีโดเมนต่างกัน
+  // app.setGlobalPrefix('api') // คือ ทุก parth ต้องมี api
+  //
+  //
   await app.listen(3000);
 }
 bootstrap();
